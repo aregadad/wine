9c6de023a525f974b620f70155ce3b9ce142da3b
aregadad <biafleks@gmail.com>
2023-04-26 21:12:44 +0300

    fix tabulation

--- main.py
+++ main.py
@@ -7,39 +7,39 @@ from environs import Env
 
 
 def get_age_ending(age):
-  if 4 < age % 100 < 21:
-    return f'{age} лет' 
-  num = age % 10
-  if num == 1:
-    return f'{age} год'
-  if 1 < num < 5:
-    return f'{age} года'
-  return f'{age} лет'
+    if 4 < age % 100 < 21:
+        return f'{age} лет'
+    num = age % 10
+    if num == 1:
+        return f'{age} год'
+    if 1 < num < 5:
+        return f'{age} года'
+    return f'{age} лет'
 
 
 if __name__ == '__main__':
-  env = Env()
-  env.read_env()
+    env = Env()
+    env.read_env()
 
-  wines_excel_path = env('WINES_EXCEL_PATH')
-  categorized_wines = defaultdict(list)
-  for wine in pd.read_excel(wines_excel_path, keep_default_na=False).to_dict(orient='records'):
-    categorized_wines[wine['Категория']].append(wine)
+    wines_excel_path = env('WINES_EXCEL_PATH')
+    categorized_wines = defaultdict(list)
+    for wine in pd.read_excel(wines_excel_path, keep_default_na=False).to_dict(orient='records'):
+        categorized_wines[wine['Категория']].append(wine)
 
-  env = Environment(loader=FileSystemLoader('.'),
-                    autoescape=select_autoescape(['html', 'xml']))
+    env = Environment(loader=FileSystemLoader('.'),
+                      autoescape=select_autoescape(['html', 'xml']))
 
-  template = env.get_template('template.html')
+    template = env.get_template('template.html')
 
-  current_year = datetime.date.today().year
-  company_foundation_year = 1920
-  rendered_page = template.render(
-    company_age=get_age_ending(current_year - company_foundation_year),
-    categorized_wines=categorized_wines
-  )
+    current_year = datetime.date.today().year
+    company_foundation_year = 1920
+    rendered_page = template.render(
+        company_age=get_age_ending(current_year - company_foundation_year),
+        categorized_wines=categorized_wines
+    )
 
-  with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
 
-  server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-  server.serve_forever()
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
